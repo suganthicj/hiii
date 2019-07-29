d68e4b5ba2a05adf94d769420a40c0211c46b4dd
suganthicj <52270901+suganthicj@users.noreply.github.com>
2019-07-29 14:27:57 +0530

    Create hiii.py

---
+++ hiii.py
@@ -0,0 +1,9 @@
+x1,y1=map(int,input().split())
+Z1=list(map(int,input().split()))
+f=l[0:x1]
+j=l[p:len(Z1)]
+a=[]
+for P in range(max(y1,x1)):
+    if f[f] in j:
+        a.append(f[P])
+a=sorted(a)
